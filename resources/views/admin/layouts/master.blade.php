580df23453db61d48e9fea72fc728a568136befa
Muhammad-alamin <alamin.softdevloper@gmail.com>
2020-10-06 04:36:40 +0600

    feature/templating done

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @include('admin.layouts._head')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    @include('admin.layouts._topNav')
+
+    @include('admin.layouts._leftNav')
+
+    @include('admin.layouts._content')
+
+    @include('admin.layouts._footer')
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+    @include('admin.layouts._jsScript')
+
+</body>
+</html>
+
